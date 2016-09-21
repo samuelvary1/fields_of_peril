c13a80cb29a57bdb2fde6ab1c626abe6a6804cef
Sam Vary <sam.vary@gmail.com>
2016-09-21 01:01:01 -0400

    meh

--- lib/input_controller.rb
+++ lib/input_controller.rb
@@ -21,7 +21,7 @@ class InputController
 
 	def input_movement(command_two)
 		direction = command_two
-		
+
 		if avatar.location.access_points && avatar.location.access_points[direction] && avatar.location.access_points[direction]["locked"]
 			if !avatar.location.access_points[direction]["visible"]
 				@current_message = "Sorry, you cannot go #{direction} from here."
