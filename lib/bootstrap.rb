f1bdad2bc0a9bd46e602e25a262fa239e4295899
Sam Vary <sam.vary@gmail.com>
2016-11-16 12:16:04 -0500

    commenting out something and add gemfile.lock

--- lib/bootstrap.rb
+++ lib/bootstrap.rb
@@ -3,7 +3,7 @@ lib_dir = File.expand_path(File.dirname(__FILE__))
 require File.join(lib_dir, 'avatar')
 require File.join(lib_dir, 'input_controller')
 require File.join(lib_dir, 'game_data_loader')
-require 'pry'
+# require 'pry'
 
 class Bootstrap
   def initialize(location_data_file, message_data_file, character_data_file, item_data_file)
